b5baf1169cc81927b9ecc1c23d2c8086efcab08e
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-10 07:28:07 -0300

    Refatorando desempenho

--- src/main/java/dev/unscrud/tdd/service/Desempenho.java
+++ src/main/java/dev/unscrud/tdd/service/Desempenho.java
@@ -1,5 +1,18 @@
 package dev.unscrud.tdd.service;
 
 public enum Desempenho {
-  A_DESEJAR, BOM, OTIMO;
+  A_DESEJAR("0.03"),
+  BOM("0.15"),
+  OTIMO("0.2");
+
+  private String porcentagem;
+
+  Desempenho(String porcentagem) {
+    this.porcentagem = porcentagem;
+  }
+
+  public String getPorcentagem() {
+    return porcentagem;
+  }
+
 }
--- src/main/java/dev/unscrud/tdd/service/ReajusteService.java
+++ src/main/java/dev/unscrud/tdd/service/ReajusteService.java
@@ -6,23 +6,7 @@ import dev.unscrud.tdd.modelo.Funcionario;
 
 public class ReajusteService {
   void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-    BigDecimal reajuste;
-
-    switch (desempenho) {
-      case A_DESEJAR:
-        reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-        break;
-      case BOM:
-        reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-        break;
-      case OTIMO:
-        reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
-        break;
-
-      default:
-        reajuste = BigDecimal.ZERO;
-    }
-
+    BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal(desempenho.getPorcentagem()));
     funcionario.reajustarSalario(reajuste);
   }
 }
