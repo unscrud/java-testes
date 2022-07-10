b9e06b1bf206357ac249a5f22cb3200dadfda8bd
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-10 08:57:03 -0300

    Refatorando testes e adiconando contagem

--- src/test/java/dev/unscrud/tdd/service/BonusServiceTest.java
+++ src/test/java/dev/unscrud/tdd/service/BonusServiceTest.java
@@ -5,18 +5,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dev.unscrud.tdd.modelo.Funcionario;
 
 public class BonusServiceTest {
+  private BonusService service;
+  private Funcionario funcionario;
+  private static int testesExecutados;
+
+  @BeforeEach
+  private void contarTestes() {
+    testesExecutados += 1;
+  }
+
+  @BeforeAll
+  private static void mostrarMensagemInicial() {
+    System.out.println("Iniciando a execução dos testes.");
+  }
+
+  @AfterAll
+  private static void mostrarContagem() {
+    System.out.println("Foram executados " + testesExecutados + " testes.");
+  }
+
+  private void inicializar(String valor) {
+    this.service = new BonusService();
+    this.funcionario = new Funcionario("Daniel", LocalDate.now(), new BigDecimal(valor));
+  }
 
   @Test
   void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
-    BonusService service = new BonusService();
-
+    inicializar("25000");
     try {
-      service.calcularBonus(new Funcionario("Daniel", LocalDate.now(), new BigDecimal("25000")));
+      service.calcularBonus(funcionario);
       fail("Não lançou a exception.");
     } catch (Exception e) {
       assertEquals("Funcionário com salário maior que R$ 10.000,00 não pode receber bonus.", e.getMessage());
@@ -26,15 +51,15 @@ public class BonusServiceTest {
 
   @Test
   void bonusDeveriaSerDezPorCentoDoSalario() {
-    BonusService service = new BonusService();
-    BigDecimal bonus = service.calcularBonus(new Funcionario("Daniel", LocalDate.now(), new BigDecimal("2500")));
+    inicializar("2500");
+    BigDecimal bonus = service.calcularBonus(funcionario);
     assertEquals(new BigDecimal("250.00"), bonus);
   }
 
   @Test
   void bonusDeveriaSerDezPorCentoDoSalarioNoLimite() {
-    BonusService service = new BonusService();
-    BigDecimal bonus = service.calcularBonus(new Funcionario("Daniel", LocalDate.now(), new BigDecimal("10000")));
+    inicializar("10000");
+    BigDecimal bonus = service.calcularBonus(funcionario);
     assertEquals(new BigDecimal("1000.00"), bonus);
   }
 }
--- src/test/java/dev/unscrud/tdd/service/ReajusteServiceTest.java
+++ src/test/java/dev/unscrud/tdd/service/ReajusteServiceTest.java
@@ -5,35 +5,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dev.unscrud.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
+  private ReajusteService service;
+  private Funcionario funcionario;
+  private static int testesExecutados;
+
+  @BeforeEach
+  private void contarTestes() {
+    testesExecutados += 1;
+  }
+
+  @BeforeAll
+  private static void mostrarMensagemInicial() {
+    System.out.println("Iniciando a execução dos testes.");
+  }
+
+  @AfterAll
+  private static void mostrarContagem() {
+    System.out.println("Foram executados " + testesExecutados + " testes.");
+  }
+
+  @BeforeEach
+  private void inicializar() {
+    service = new ReajusteService();
+    funcionario = new Funcionario("Daniel", LocalDate.now(), new BigDecimal("1000.00"));
+  }
+
   @Test
   void reajusteDeveriaSerDe3PorcentoSeDesempenhoForADesejar() {
-    ReajusteService service = new ReajusteService();
-    Funcionario funcionario = new Funcionario("Daniel", LocalDate.now(), new BigDecimal("1000.00"));
     service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
-
     assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
   }
 
   @Test
   void reajusteDeveriaSerDe15PorcentoSeDesempenhoForBom() {
-    ReajusteService service = new ReajusteService();
-    Funcionario funcionario = new Funcionario("Daniel", LocalDate.now(), new BigDecimal("1000.00"));
     service.concederReajuste(funcionario, Desempenho.BOM);
-
     assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
   }
 
   @Test
   void reajusteDeveriaSerDe20PorcentoSeDesempenhoForBom() {
-    ReajusteService service = new ReajusteService();
-    Funcionario funcionario = new Funcionario("Daniel", LocalDate.now(), new BigDecimal("1000.00"));
     service.concederReajuste(funcionario, Desempenho.OTIMO);
-
     assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
   }
 
